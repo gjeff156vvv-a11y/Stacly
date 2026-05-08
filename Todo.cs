7e00fcadb845d76a76c53be4b706f78480f355ae
Роман Матвий <gjeff156vvv@gmail.com>
2026-05-08 16:44:10 +0300

    refactor: update Todo model with CompletedAt and List of Tags

--- Todo.cs
+++ Todo.cs
@@ -9,13 +9,14 @@ namespace TodoList
 {
     public class Todo
     {
-        public string      Name        {get;private set;}
-        public string      Description {get;private set;}
-        public bool        IsCompleted {get;private set;}
-        public DateTime    CreatedAt   {get;private set;}
-        public Priorities  Priority    {get;private set;}
-        public string      Tags        {get;private set;}
-        public List<Todo>  SubTasks    {get;private set;}
+        public string       Name        {get;private set;}
+        public string       Description {get;private set;}
+        public bool         IsCompleted {get;private set;}
+        public DateTime     CreatedAt   {get;private set;}
+        public DateTime?    CompleteAt  {get;private set;}
+        public Priorities   Priority    {get;private set;}
+        public List<string> Tags        {get;private set;}
+        public List<Todo>   SubTasks    {get;private set;}
 
         public Todo(string name,Priorities priority)
         {
@@ -24,17 +25,18 @@ namespace TodoList
             IsCompleted = false;
             CreatedAt = DateTime.Now;
             Priority = priority;
-            Tags = " ";
+            Tags = new List<string>();
             SubTasks = new List<Todo>();
         }
 
         [JsonConstructor]
-        public Todo(string name,string description,bool isCompleted,DateTime createdAt,Priorities priority,string tags,List<Todo> subTasks)
+        public Todo(string name,string description,bool isCompleted,DateTime createdAt,DateTime? completeAt,Priorities priority,List<string> tags,List<Todo> subTasks)
         {
             Name = name;
             Description = description;
             IsCompleted = isCompleted;
             CreatedAt = createdAt;
+            CompleteAt = completeAt;
             Priority = priority;
             Tags = tags;
             SubTasks = subTasks;
@@ -50,18 +52,26 @@ namespace TodoList
         }
         public void ChangeIsCompleted()
         {
-            IsCompleted = !IsCompleted;
-            if(SubTasks.Count == 0 || SubTasks == null)
-            {
-                return ;
-            }
-            else 
+            ChangeIsCompleted(!IsCompleted);
+        }
+
+        public void ChangeIsCompleted(bool status)
+        {
+            IsCompleted = status;
+            if(SubTasks.Count > 0)
             {
                 foreach(var obj in SubTasks)
                 {
                     obj.ChangeIsCompleted();
                 }
             }
+
+            switch(status)
+            {
+                case false:CompleteAt = null;break;
+                case true: CompleteAt = DateTime.Now;break;
+            }
+
         }
         public void ChangePriority(Priorities num)
         {
@@ -69,7 +79,7 @@ namespace TodoList
         }
         public void ChangeTags(string newTag)
         {
-            Tags = newTag;
+            Tags = newTag.Split(",").ToList();
         }
     }
 }
